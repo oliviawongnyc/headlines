799eed6af1e650599b5b32dd4b22d78d02756f88
Olivia Wong <oliviawong@nypl.org>
2024-02-02 16:45:33 -0500

    Adds confetti

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import { Button, Flex, Heading } from "@chakra-ui/react";
+import Realistic from "react-canvas-confetti/dist/presets/realistic";
+import { TConductorInstance } from "react-canvas-confetti/dist/types";
 import { useHeadline } from "../hooks/useHeadline";
 import { useScore } from "../hooks/useScore";
 import { QUESTION_COUNT } from "../data/constants";
 
 const GameOver = () => {
+  const [conductor, setConductor] = useState<TConductorInstance>();
+
   const { setCurrentHeadlineIdx } = useHeadline();
   const { score, setScore } = useScore();
+
+  const onInit = ({ conductor }: { conductor: TConductorInstance }) => {
+    setConductor(conductor);
+  };
 
   const resetGame = () => {
     setCurrentHeadlineIdx(0);
@@ -13,11 +22,14 @@ const GameOver = () => {
   };
 
   const scoreIcon = () => {
-    if (score === QUESTION_COUNT) return "🥳";
-    else if (score > QUESTION_COUNT - 2) return "🥂";
+    if (score === QUESTION_COUNT) {
+      conductor?.shoot();
+      return "🥳";
+    } else if (score > QUESTION_COUNT - 2) return "🥂";
     else if (score >= QUESTION_COUNT - 5) return "😊";
     else return "🫣";
   };
+
   return (
     <Flex
       border="1px solid lightGray"
@@ -31,6 +43,7 @@ const GameOver = () => {
         You answered {score} out of {QUESTION_COUNT} correctly! {scoreIcon()}
       </Heading>
       <Button onClick={resetGame}>Play again</Button>
+      <Realistic onInit={onInit} />
     </Flex>
   );
 };
