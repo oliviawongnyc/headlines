b1799317851a574b33cd6ab1b27f78b37cfd6d21
Olivia Wong <oliviawong@nypl.org>
2024-02-07 10:15:24 -0500

    Cleans up

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,4 +1,1 @@
 export const QUESTION_COUNT = 10;
-export const SUPABASE_URL = "https://dxwcjiihxfudjgymgwpw.supabase.co";
-export const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4d2NqaWloeGZ1ZGpneW1nd3B3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyNDgwMzQsImV4cCI6MjAyMjgyNDAzNH0.yZbk3NagwK06MTqI9ksApJo7KavMfeTzmlb-2CxgPck";
